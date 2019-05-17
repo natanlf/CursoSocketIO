0d624e610cbcefede6c4ff7e0f24b47427030120
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2019-05-17 14:39:58 -0300

    Chat basico

---
+++ chat.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const socketio = require('socket.io')
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+
+//SOCKETIO RECEBE O SERVIDOR EXPRESS
+const io = socketio(expressServer);
+io.on('connection',(socket)=>{
+    socket.emit('messageFromServer',{data:"Welcome to the socketio server"});
+    socket.on('messageToServer',(dataFromClient)=>{
+        console.log(dataFromClient)
+    })
+    socket.on('newMessageToServer',(msg)=>{
+        // console.log(msg)
+        io.emit('messageToClients',{text:msg.text})
+    })
+})
+
